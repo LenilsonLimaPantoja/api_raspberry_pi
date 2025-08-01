9edeb1b00b0384645ad49fa3331ce75cd4abfdd2
LenilsonLimaPantoja <lenilsonlm.pantoja@gmail.com>
2025-08-01 11:12:51 -0400

    first commit

--- test_exec.js
+++ test_exec.js
@@ -1,14 +1,16 @@
 const { execFile } = require('child_process');
-const path = '/home/pi/Desktop/api/src/scripts/configurar_wifi.sh';
 
-const ssid = 'SuaRede';
-const password = 'SuaSenha';
+const path = '/home/pi/Desktop/api/src/scripts/configurar_wifi.sh';
+const ssid = 'GE-CLARO';
+const password = 'Gem5a970**';
 
 execFile('sudo', [path, ssid, password], (error, stdout, stderr) => {
     if (error) {
-        console.error('Erro ao executar script:', error.message);
+        console.error('Erro ao executar o script:', error.message);
         console.error('stderr:', stderr);
         return;
     }
+
     console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
 });
